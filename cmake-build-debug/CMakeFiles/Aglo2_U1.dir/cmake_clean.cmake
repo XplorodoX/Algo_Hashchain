735754e4943e0fcf56514c686b65974c943940ab
Florian Merlau <merlaufl@gmail.com>
2021-11-08 13:44:28 +0100

    V1.0

---
+++ cmake-build-debug/CMakeFiles/Aglo2_U1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Aglo2_U1.dir/main.cpp.o"
+  "Aglo2_U1.pdb"
+  "Aglo2_U1"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Aglo2_U1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
